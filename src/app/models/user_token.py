f40b3c4e63f96c13e312debb438a982a1f5a7973
Salomon Metre <kulosmetros088@gmail.com>
2025-05-31 19:13:30 +0300

    update user_token model

--- src/app/models/user_token.py
+++ src/app/models/user_token.py
@@ -1,13 +1,16 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
 from sqlalchemy.orm import relationship
 from app.models.base import Base
 
 class UserToken(Base):
     __tablename__ = "user_tokens"
+
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     access_token = Column(String, nullable=False)
     refresh_token = Column(String, nullable=False)
-    access_token_expiry = Column(DateTime(timezone=True), nullable=False)  # Added timezone=True
-    refresh_token_expiry = Column(DateTime(timezone=True), nullable=False)  # Added timezone=True
+    access_token_expiry = Column(DateTime(timezone=True), nullable=False)
+    refresh_token_expiry = Column(DateTime(timezone=True), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
     user = relationship("User", back_populates="tokens")
