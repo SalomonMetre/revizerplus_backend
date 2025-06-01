ec077412abb05b4bdf94cb71791336629dbe5724
Salomon Metre <kulosmetros088@gmail.com>
2025-06-01 18:24:59 +0000

    update main.py to solve CORS error

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import user
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://revizerplus.web.app/"],  # Replace with your frontend's origin
+    allow_credentials=True,  # Allow cookies or auth headers if needed
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers (e.g., Content-Type, Authorization)
+)
 
 app.include_router(user.router)
