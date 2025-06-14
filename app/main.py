f6bfce748e9c705ba5bf18b5deb2d0795cef1708
Salomon Metre <kulosmetros088@gmail.com>
2025-06-14 14:14:37 +0000

    update main.py to fix CORS issue

--- app/main.py
+++ app/main.py
@@ -1,23 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware import Middleware
 from core.config import settings
 from auth.routes import router as auth_router
 from users.routes import router as user_router
+
+# Configure middleware
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=False,  # Set to False to allow wildcard origins
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
+]
 
 app = FastAPI(
     title="Revizer Plus API",
     debug=settings.APP_DEBUG,
     docs_url="/docs",
     redoc_url="/redoc",
-)
-
-# CORS configuration
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    middleware=middleware
 )
 
 # Include routers
